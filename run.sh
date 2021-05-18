6d0b829466356dfc26ae10218098d52c741380ea
AkashCba <akashcba2022@email.iimcal.ac.in>
2021-05-18 19:55:17 +0530

    2nd Day Commit

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 export TRAINING_DATA="/Users/my_mac/Documents/Machine Learning/ML/input/train_folds.csv"
-export FOLD="0"
+export TEST_DATA="/Users/my_mac/Documents/Machine Learning/ML/input/test.csv"
+export MODEL_PATH="/Users/my_mac/Documents/Machine Learning/ML/models"
 
 export MODEL=$1
 
-python -m src.train
+FOLD=0 python -m src.train
+FOLD=1 python -m src.train
+FOLD=2 python -m src.train
+FOLD=3 python -m src.train
+FOLD=4 python -m src.train
+
+python -m src.predict
--- src/train.py
+++ src/train.py
@@ -9,6 +9,7 @@ from . import dispatcher
 import joblib
 
 TRAINING_DATA = os.environ.get("TRAINING_DATA")
+TEST_DATA = os.environ.get("TEST_DATA")
 FOLD = int(os.environ.get("FOLD"))
 MODEL = os.environ.get("MODEL")
 
@@ -22,8 +23,9 @@ Fold_Mapping = {
 
 if __name__ == "__main__":
     df = pd.read_csv(TRAINING_DATA)
-    train_df = df[df.kfold.isin(Fold_Mapping.get(FOLD))]
-    valid_df = df[df.kfold==FOLD]
+    df_test = pd.read_csv(TEST_DATA)
+    train_df = df[df.kfold.isin(Fold_Mapping.get(FOLD))].reset_index(drop=True)
+    valid_df = df[df.kfold==FOLD].reset_index(drop=True)
 
     ytrain = train_df.target.values
     yvalid = valid_df.target.values
@@ -31,26 +33,32 @@ if __name__ == "__main__":
     train_df = train_df.drop(["id", "target", "kfold"], axis=1)
     valid_df = valid_df.drop(["id", "target", "kfold"], axis=1)
 
+    print("\nTraining Fold : ", FOLD, "\n")
+
 ## Add Same column sequence in both
     valid_df = valid_df[train_df.columns]
 
-    label_encoders =[]
+    label_encoders ={}
     for c in train_df.columns:
         lbl = preprocessing.LabelEncoder()
-        lbl.fit(train_df[c].values.tolist() + valid_df[c].values.tolist())
-        train_df.loc[:,c] = lbl.transform(train_df[c].values.tolist())
-        valid_df.loc[:,c] = lbl.transform(valid_df[c].values.tolist())
-        label_encoders.append((c, lbl))
+        train_df.loc[:, c] = train_df.loc[:, c].astype(str).fillna("NONE")
+        valid_df.loc[:, c] = valid_df.loc[:, c].astype(str).fillna("NONE")
+        df_test.loc[:, c] = df_test.loc[:, c].astype(str).fillna("NONE")
+        lbl.fit(train_df[c].values.tolist() + 
+                valid_df[c].values.tolist() +
+                df_test[c].values.tolist())
 
+        train_df.loc[:, c] = lbl.transform(train_df[c].values.tolist())
+        valid_df.loc[:, c] = lbl.transform(valid_df[c].values.tolist())
+        label_encoders[c] = lbl
     # Data is ready to train
     clf = dispatcher.MODELS[MODEL]
     clf.fit(train_df, ytrain)
     preds = clf.predict_proba(valid_df)[:, 1]
     #print(preds)
-    print(metrics.roc_auc_score(yvalid, preds))
+    print("Accuracy : ", metrics.roc_auc_score(yvalid, preds))
 
     ## Storing the data for predict.py
     joblib.dump(label_encoders, f"/Users/my_mac/Documents/Machine Learning/ML/models/{MODEL}_{FOLD}_label_encoder.pkl")
     joblib.dump(clf, f"/Users/my_mac/Documents/Machine Learning/ML/models/{MODEL}_{FOLD}.pkl")
     joblib.dump(train_df.columns, f"/Users/my_mac/Documents/Machine Learning/ML/models/{MODEL}_{FOLD}_columns.pkl")
-
